ed4a552942893f6a3dd6c67620b82e728bbe71f0
Sridhar Ratnakumar <srid@srid.ca>
2015-06-01 23:21:38 -0400

    add function to make logplex frame

--- record.go
+++ record.go
@@ -3,6 +3,7 @@ package drain
 import (
 	"fmt"
 	"github.com/bmizerany/lpx"
+	"strings"
 )
 
 // Record represents a log line processed by drains
@@ -22,4 +23,23 @@ func (r Record) String() string {
 		string(r.Header.Msgid),
 		string(r.Data))
 	return line
+}
+
+// 70 <174>1 2012-07-22T00:06:26+00:00 host erlang console - Hi from erlang
+// Make a logplex POST body frame from multiple records
+func MakeLogplexFrame(records []*Record) string {
+	lines := make([]string, 0, len(records))
+	for _, record := range records {
+		line := fmt.Sprintf("%s %s %s %s %s %s %s\n",
+			record.Header.PrivalVersion,
+			record.Header.Time,
+			record.Header.Hostname,
+			record.Header.Name,
+			record.Header.Procid,
+			record.Header.Msgid,
+			string(record.Data))
+		line = fmt.Sprintf("%d %s", len(line), line)
+		lines = append(lines, line)
+	}
+	return strings.Join(lines, "")
 }
