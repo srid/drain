574bb366e2f018c047a5f570c4ba6cc6b37d433e
Sridhar Ratnakumar <srid@srid.ca>
2015-06-01 23:02:34 -0400

    Fix all compile errors

--- cmd/drainsrv/main.go
+++ cmd/drainsrv/main.go
@@ -10,25 +10,18 @@ import (
 	"net/http"
 )
 
-func NewGinRouter() gin.Accounts {
-	return gin.Accounts{
-		config.Username: config.Password,
-	}
-}
-
 func main() {
 	var routerGroup *gin.RouterGroup
 	router := gin.Default()
 
-	username, password, authorize = config.GetUserPass()
+	username, password, authorize := config.GetUserPass()
 	if authorize {
 		routerGroup = router.Group("/",
 			gin.BasicAuth(gin.Accounts{username: password}))
 	} else {
-		routerGroup = router
+		routerGroup = router.Group("/")
 	}
 
-	routerGroup := router.Group("/", gin.BasicAuth(makeGinAccounts()))
 	routerGroup.POST("/logs", logsReceived)
 	log.Printf("Running drain server at port %v\b", config.Port)
 	router.Run(":" + config.Port)
